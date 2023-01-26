25584b59db64d6c8e902ca5378e207ac68ea374f
pangphannarupp <pangphannarupp@gmail.com>
2023-01-26 10:10:42 +0700

    Initial Commit

---
+++ khmer_dictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  khmer_dictionary
+//
+//  Created by 임재욱 on 26/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ khmer_dictionary/khmer_dictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  khmer_dictionaryApp.swift
+//  khmer_dictionary
+//
+//  Created by 임재욱 on 26/1/23.
+//
+
+import SwiftUI
+
+@main
+struct khmer_dictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
